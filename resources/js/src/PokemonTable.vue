aaf695ddac3074a29a020e3d15f1e93045030964
khorramk <khorramk.kbsk@gmail.com>
2021-02-10 19:08:01 +0000

    poekom table component

--- resources/js/src/PokemonTable.vue
+++ resources/js/src/PokemonTable.vue
@@ -1,51 +1,84 @@
 <template>
-    <table class="table table-sm table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">name</th>
-      <th scope="col">height</th>
-      <th scope="col">weight</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">
+                    #1
+                </th>
+                <th>
+                    picture
+                </th>
+                <th>
+                    name
+                </th>
+                <th>
+                    weight
+                </th>
+                <th>
+                    height
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(monster, i) in pokemon" :key="i">
+                <td>
+                    {{i}}
+                </td>
+                <td>
+                    <img :src="monster.imgUrl" :alt="monster.name">
+                </td>
+                <td>
+                    {{monster.name}}
+                </td>
+                <td>
+                    {{monster.weight}}
+                </td>
+                <td>
+                    {{monster.height}}
+                </td>
+            </tr>
+        </tbody>
+
+    </table>
 </template>
 
 <script>
 export default {
     data(){
         return {
-            pokemons: []
+            pokemons: [],
+            pokemonsURl: [],
+            pokemon: []
         };
     },
     mounted() {
         axios.get('/api/pokemons')
             .then((response) => {
-                console.log(response);
-            });
+                // console.log(response);
+                this.pokemonsURl = response.data.data;
+                this.runPokemonRequest();
+            })
+
+    },
+    methods: {
+        runPokemonRequest() {
+            for (let index = 0; index < this.pokemonsURl.length; index++) {
+                const url = this.pokemonsURl[index]['url'];
+                console.log(url);
+                axios.get(url).then(resp => {
+                    console.log(resp);
+                    let pokemon = {};
+                    pokemon.name = this.pokemonsURl[index]['name'];
+
+                    pokemon.weight = resp.data.weight;
+                    pokemon.height = resp.data.height;
+                    pokemon.imgUrl = resp.data.sprites.back_default;
+                    this.pokemon.push(pokemon);
+                });
+
+            }
+        }
     }
 }
 </script>
 
-<style>
-
-</style>
