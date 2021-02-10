a77abef8ff8fb7ca2f4224fd24b9e2f8b0f621a6
khorramk <khorramk.kbsk@gmail.com>
2021-02-10 17:18:08 +0000

    pokemon table created

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Vue from 'vue';
+import PokemonTable from './src/PokemonTable.vue';
+
+new Vue({
+    el: '#root',
+    template: 'hello',
+    components: {
+        PokemonTable,
+    },
+});
